1860ac392649b8a7ca7da283ecd0c84e876ecf8e
Ruan Herculano <ruannawe95@gmail.com>
2020-01-02 15:14:34 -0300

    Troca nome das variáveis

--- main.go
+++ main.go
@@ -16,24 +16,24 @@ type User struct {
 type Users []User
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", HandleFunc).Methods("GET")
+	router := mux.NewRouter()
+	router.HandleFunc("/", HandleFunc).Methods("GET")
 
 	headersOk := handlers.AllowedHeaders([]string{"*"})
 	originsOk := handlers.AllowedOrigins([]string{"*"})
 	methodsOk := handlers.AllowedMethods([]string{"*"})
 
-	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router))
 }
 
-func HandleFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+func HandleFunc(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	responseWriter.WriteHeader(http.StatusOK)
 	
     users := Users{
         User{Name: "Write presentation", Active: false},
         User{Name: "Host meetup", Active: false},
     }
 
-	json.NewEncoder(w).Encode(users);
+	json.NewEncoder(responseWriter).Encode(users);
 }
